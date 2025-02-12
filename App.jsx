023427381b874d05fa022ac3c62a39e25ea44f24
22016304-Jovan <134572451+22016304-Jovan@users.noreply.github.com>
2025-02-12 10:47:29 +0800

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
+import ProtectedRoute from "./components/ProtectedRoute";
+import UserDashboard from "./routes/UserDashboard";
+import StaffDashboard from "./routes/StaffDashboard";
+import Register from "./pages/Register";
+import ViewCart from "./user/ViewCart";
+
+function LogOut() {
+  localStorage.clear();
+  return <Navigate to="/login" replace={true} />;
+}
+
+function RegisterandLogOut() {
+  localStorage.clear();
+  return <Register />;
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/admin-Dashboard/*"
+          element={
+            <ProtectedRoute allowedRoles={['Admin']}>
+              <StaffDashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/user-Dashboard/*"
+          element={
+            <ProtectedRoute allowedRoles={['User']}>
+              <UserDashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/cart" element={<ViewCart />} />
+        <Route path="/logout" element={<LogOut />} />
+        <Route path="/register" element={<RegisterandLogOut />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
